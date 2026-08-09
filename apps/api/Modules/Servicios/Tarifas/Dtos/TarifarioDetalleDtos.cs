a77d5f3b11aa74dd9173bf0acd192d29fe3c3939
AleMicky <miguel.ale19934@gmail.com>
2026-08-08 22:02:09 -0400

    feat: add endpoint to bulk-add tarifario details by service category

---
+++ apps/api/Modules/Servicios/Tarifas/Dtos/TarifarioDetalleDtos.cs
@@ -0,0 +1,6 @@
+namespace Clinica.Api.Modules.Servicios.Tarifas.Dtos;
+
+public class TarifarioDetalleDtos
+{
+    
+}
---
+++ apps/api/Modules/Servicios/Tarifas/Validators/TarifarioDetalleCategoriaRequestValidator.cs
@@ -0,0 +1,6 @@
+namespace Clinica.Api.Modules.Servicios.Tarifas.Validators;
+
+public class TarifarioDetalleCategoriaRequestValidator
+{
+    
+}
