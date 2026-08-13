83da2dc85426444ba9d3920161ced087f005ac37
AleMicky <miguel.ale19934@gmail.com>
2026-08-12 22:02:30 -0400

    feat: add Correlativo module and refactor AdmisionService to manual CRUD implementation

---
+++ apps/api/Modules/Parametros/Correlativo/Endpoints/CorrelativoEndpoints.cs
@@ -0,0 +1,6 @@
+namespace Clinica.Api.Modules.Parametros.Correlativo.Endpoints;
+
+public class CorrelativoEndpoints
+{
+    
+}
---
+++ apps/api/Modules/Parametros/Correlativo/Services/CorrelativoService.cs
@@ -0,0 +1,6 @@
+namespace Clinica.Api.Modules.Parametros.Correlativo.Services;
+
+public class CorrelativoService
+{
+    
+}
