cd7628af14a62762770b43fd8373d4eaa8197cc6
AleMicky <miguel.ale19934@gmail.com>
2026-09-01 11:01:20 -0400

    feat: implement BajaInventario module and improve patient record handling in admission form

---
+++ apps/api/Modules/Almacenes/BajaInventario/Entity/BajaInventario.cs
@@ -0,0 +1,6 @@
+namespace Clinica.Api.Modules.Almacenes.BajaInventario.Entity;
+
+public class BajaInventario
+{
+    
+}
---
+++ apps/api/Modules/Almacenes/BajaInventario/Enums/EstadoBajaInventario.cs
@@ -0,0 +1,6 @@
+namespace Clinica.Api.Modules.Almacenes.BajaInventario.Enums;
+
+public enum EstadoBajaInventario
+{
+    
+}
