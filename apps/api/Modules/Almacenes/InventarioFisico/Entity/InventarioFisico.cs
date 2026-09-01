a8269ef7efa95f1c2b056fd96a77cd51dceefc2a
AleMicky <miguel.ale19934@gmail.com>
2026-09-01 09:47:52 -0400

    feat: implement inventory physical count system and warehouse transfer management module

---
+++ apps/api/Modules/Almacenes/InventarioFisico/Entity/InventarioFisico.cs
@@ -0,0 +1,6 @@
+namespace Clinica.Api.Modules.Almacenes.InventarioFisico.Entity;
+
+public class InventarioFisico
+{
+    
+}
---
+++ apps/api/Modules/Almacenes/InventarioFisico/Enums/EstadoInventarioFisico.cs
@@ -0,0 +1,6 @@
+namespace Clinica.Api.Modules.Almacenes.InventarioFisico.Enums;
+
+public enum EstadoInventarioFisico
+{
+    
+}
